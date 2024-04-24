9d970a347efdaacfdf1860b04c123688db902284
Daniel Rangel <danbar.dg@gmail.com>
2024-04-23 22:06:55 -0600

    este es mi primer commit

---
+++ script.js
@@ -0,0 +1,83 @@
+let movies = [
+    {
+        name: "Ruffles Queso",
+        id: 663712,
+        contNet: 175,
+        medida: "gramos",
+        presentacion: "Bolsa",
+        boxCont: 24,
+        caducidad: '2024-12-06',
+        inventario: 458,
+        precioRet: 34,
+        precioVent: 58,
+        precioVentMay: 42
+    },
+    {
+        name: "Nachos salados",
+        id: 343712,
+        contNet: 198,
+        medida: "gramos",
+        presentacion: "Bolsa",
+        boxCont: 12,
+        caducidad: '2024-09-06',
+        inventario: 35,
+        precioRet: 63,
+        precioVent: 70,
+        precioVentMay: 66
+    },
+    {
+        name: "CocaCola",
+        id: 343723,
+        contNet: 600,
+        medida: "mililitros",
+        presentacion: "botella",
+        boxCont: 12,
+        caducidad: '2024-03-06',
+        inventario: 35,
+        precioRet: 63,
+        precioVent: 70,
+        precioVentMay: 66
+    },
+    {
+        name: "Maruchan",
+        id: 331812,
+        contNet: 60,
+        medida: "gramos",
+        presentacion: "vaso",
+        boxCont: 24,
+        caducidad: '2025-02-18',
+        inventario: 79,
+        precioRet: 9,
+        precioVent: 15,
+        precioVentMay: 12
+    },
+    {
+        name: "Old Space",
+        id: 750630,
+        contNet: 150,
+        medida: "mililitros",
+        presentacion: "aerosol",
+        boxCont: 8,
+        caducidad: '2026-05-14',
+        inventario: 349,
+        precioRet: 30,
+        precioVent: 49,
+        precioVentMay: 40
+    },
+    {
+        name: "Old Space",
+        id: 750043,
+        contNet: 50,
+        medida: "gramos",
+        presentacion: "barra",
+        boxCont: 18,
+        caducidad: '2025-09-23',
+        inventario: 233,
+        precioRet: 21,
+        precioVent: 34,
+        precioVentMay: 26
+    }
+]
+
+
+
